c98053aa29a9e9a4af2dbc3f97f05d70017cd31b
RofifAthoyaNurPrabu <rofif.athoya.ft19@mail.umy.ac.id>
2022-03-27 22:52:18 +0700

    New Post

---
+++ app/src/main/java/com/example/exercise1/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.exercise1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin, btnRegister;
+    EditText eduser, edpassword;
+    String nama,password;
+
+
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin=findViewById(R.id.btsignin);
+        btnRegister=findViewById(R.id.btnRegister);
+        eduser=findViewById(R.id.eduser);
+        edpassword=findViewById(R.id.edpassword);
+
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nama = eduser.getText().toString();
+                password = edpassword.getText().toString();
+
+                String name = "Andre";
+                String pass = "iniPass";
+
+                if (nama.isEmpty() || password.isEmpty()) {
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Nama dan Password wajib diisi!!!",
+                            Toast.LENGTH_LONG);
+                } else {
+                    if (nama.equals(name) && password.equals(pass)) {
+                        Toast t = Toast.makeText(getApplicationContext(),
+                                "Login Sukses",
+                                Toast.LENGTH_LONG);
+                        t.show();
+
+                        Bundle b = new Bundle();
+                        b.putString("a", nama.trim());
+
+                        Intent i = new Intent(getApplicationContext(), ActivityKedua.class);
+                        i.putExtras(b);
+                        startActivity(i);
+
+                    } else {
+                        if (nama.isEmpty() || password.equals(pass)) {
+                            Toast t = Toast.makeText(getApplicationContext(),
+                                    "Nama salah",
+                                    Toast.LENGTH_LONG);
+                            t.show();
+                        } else {
+                            if (nama.equals(name)) {
+                                Toast t = Toast.makeText(getApplicationContext(),
+                                        "Password salah",
+                                        Toast.LENGTH_LONG);
+                                t.show();
+                            }
+                        }
+
+
+                    }
+                }
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), Register.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
